50f1c45ba3bc1a37c2fa6d468005d0db83e39e4e
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2023-02-22 21:31:28 -0500

    chore: add head an icon

---
+++ app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <title>Advisor</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <link rel="icon" href="/quote.svg" type="image/svg+xml" />
+    </>
+  )
+}
