d2f42b18bfbc491b1cae0a98887a7920c512680e
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2023-02-28 18:38:22 -0500

    feat: component to add a new quote

---
+++ components/createQuote/index.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { useState } from "react";
+// import { Quote } from "../../shared/quotesInterface";
+import { addQuote } from "../../firebase/client";
+import styles from "./styles.module.css";
+
+export function CreateQuote() {
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
+
+  const handleSubmit = async () => {
+    const newQuote = { quote, author };
+    await addQuote(newQuote);
+    setQuote("");
+    setAuthor("");
+    window.location.reload();
+  };
+
+  return (
+    <div className={styles.container}>
+      <input
+        className={styles.input}
+        type="text"
+        placeholder="Add your quote here"
+        onChange={(e) => setQuote(e.target.value)}
+        value={quote}
+        autoFocus
+      />
+      <input
+        className={styles.inputAuthor}
+        type="text"
+        placeholder="author here"
+        onChange={(e) => setAuthor(e.target.value)}
+        value={author}
+      />
+      <button onClick={handleSubmit} className={styles.button}>
+        save
+      </button>
+    </div>
+  );
+}
