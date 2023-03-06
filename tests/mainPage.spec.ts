70c00dab8cd06df1650743fbbc46c81d7f7f47e9
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2023-03-06 17:20:44 -0500

    test: new test to check the change of quotes with the button

--- tests/mainPage.spec.ts
+++ tests/mainPage.spec.ts
@@ -14,3 +14,25 @@ test("app shows quote and author", async ({ page }) => {
   await expect(textContent?.length).toBeGreaterThan(0);
   await expect(titleContent?.length).toBeGreaterThan(0);
 });
+
+test("should change the quote and author when the button is clicked", async ({ page }) => {
+  await page.goto(LOCALHOST_URL);
+
+  const text = await page.getByRole("paragraph");
+  const title = await page.getByRole("heading");
+
+  const initialQuote = await text.textContent();
+  const initialAuthor = await title.textContent();
+
+  await page.click('button:has-text("Change Quote")');
+  await page.waitForTimeout(2000);
+
+  const newText = await page.getByRole("paragraph");
+  const newTitle = await page.getByRole("heading");
+
+  const newQuote = await newText.textContent();
+  const newAuthor = await newTitle.textContent();
+
+  expect(newQuote).not.toBe(initialQuote);
+  expect(newAuthor).not.toBe(initialAuthor);
+});
