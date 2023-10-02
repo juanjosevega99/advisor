872d05e6e1d67bf6ea6d91994ed8a36a7dc5b45c
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2023-10-01 22:42:43 -0500

    test: two options for testing

---
+++ tests/quotesPage.spec.ts
@@ -0,0 +1,12 @@
+import { test, expect } from "@playwright/test";
+
+const LOCALHOST_URL = "http://localhost:3000/quotes";
+
+test("should render the cards with quote and author", async ({ page }) => {
+
+});
+
+
+test("should add a new quote when the button save is clicked", async ({ page }) => {
+
+});
