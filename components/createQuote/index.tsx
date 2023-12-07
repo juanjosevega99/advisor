d6883809a6b466e6fe93e135cacdfc8c4081466a
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2023-12-07 12:01:05 -0500

    refactor: trim quote and author

--- components/createQuote/index.tsx
+++ components/createQuote/index.tsx
@@ -1,41 +1,67 @@
-"use client";
+'use client';
 
-import { useState } from "react";
-import { addQuote } from "../../firebase/client";
-import styles from "./styles.module.css";
+import { useState } from 'react';
+import { addQuote } from '../../firebase/client';
+import styles from './styles.module.css';
+import Notification from '../Notification';
 
 export function CreateQuote() {
-  const [quote, setQuote] = useState("");
-  const [author, setAuthor] = useState("");
+  const [quote, setQuote] = useState('');
+  const [author, setAuthor] = useState('');
+  const [message, setMessage] = useState('');
+  const [messageType, setMessageType] = useState<'error' | 'success' | null>(
+    null
+  );
 
   const handleSubmit = async () => {
-    const newQuote = { quote, author };
-    await addQuote(newQuote);
-    setQuote("");
-    setAuthor("");
-    window.location.reload();
+    const newQuote = {
+      quote: quote.trim(),
+      author: author.trim(),
+    };
+
+    if (!newQuote.quote || !newQuote.author) {
+      setMessage('Please enter both a quote and an author.');
+      setMessageType('error');
+      return;
+    }
+
+    const response = await addQuote(newQuote);
+
+    if (response.success) {
+      setMessage(response.message);
+      setMessageType('success');
+      setQuote('');
+      setAuthor('');
+      window.location.reload();
+    } else {
+      setMessage(response.message);
+      setMessageType('error');
+    }
   };
 
   return (
-    <div className={styles.container}>
-      <input
-        className={styles.input}
-        type="text"
-        placeholder="Add your quote here"
-        onChange={(e) => setQuote(e.target.value)}
-        value={quote}
-        autoFocus
-      />
-      <input
-        className={styles.inputAuthor}
-        type="text"
-        placeholder="author here"
-        onChange={(e) => setAuthor(e.target.value)}
-        value={author}
-      />
-      <button onClick={handleSubmit} className={styles.button}>
-        save
-      </button>
+    <div>
+      <Notification message={message} type={messageType} />
+      <div className={styles.container}>
+        <input
+          className={styles.input}
+          type='text'
+          placeholder='Add your quote here'
+          onChange={(e) => setQuote(e.target.value)}
+          value={quote}
+          autoFocus
+        />
+        <input
+          className={styles.inputAuthor}
+          type='text'
+          placeholder='author here'
+          onChange={(e) => setAuthor(e.target.value)}
+          value={author}
+        />
+        <button onClick={handleSubmit} className={styles.button}>
+          save
+        </button>
+      </div>
     </div>
   );
 }
