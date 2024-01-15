b445a4d0bb15a867600a2e81a89cb187e73440d7
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2024-01-14 21:02:26 -0500

    test: refactor jest ts files

--- jest.config.ts
+++ jest.config.ts
@@ -1,17 +1,18 @@
-const nextJest = require('next/jest')
+import type { Config } from 'jest';
+const nextJest = require('next/jest');
 
 const createJestConfig = nextJest({
   dir: './',
-})
+});
 
-const config = {
+const config: Config = {
   clearMocks: true,
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   testEnvironment: 'jest-environment-jsdom',
   preset: 'ts-jest',
   testPathIgnorePatterns: ['<rootDir>/tests'],
 };
 
-module.exports = createJestConfig(config)
+export default createJestConfig(config);
