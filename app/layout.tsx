6e6f5f8742a78ea8284e0c375947dc710f1d3f54
juan <juan.vega@hqo.co>
2025-03-29 16:46:50 -0500

    styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,14 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html>
-      <head />
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
+        <meta name="theme-color" content="#340a3f" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+      </head>
       <body>{children}</body>
     </html>
   )
